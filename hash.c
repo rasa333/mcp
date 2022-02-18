fc07ceb4eecc7dbbd6ae5baa357c066259f728c0
hej <h.jappe@gmail.com>
2022-02-18 16:13:18 +0100

    fixes

--- hash.c
+++ hash.c
@@ -10,101 +10,100 @@
 
 inline static unsigned hash_make(char *s)
 {
-  unsigned hashval;
+    unsigned hashval;
 
-  for (hashval = 0 ; *s != 0 ; s++) 
-    hashval = *s + 31 * hashval;
-  
-  return hashval % HASHSIZE;
+    for (hashval = 0; *s != 0; s++)
+        hashval = *s + 31 * hashval;
+
+    return hashval % HASHSIZE;
 }
 
 
 HASH *hash_lookup(HASH **hashtab, char *ipstr)
 {
-  HASH *np;
+    HASH *np;
 
-  for (np = hashtab[hash_make(ipstr)] ; np != NULL ; np = np->next) {
-    if (!strcmp(np->ipstr, ipstr))
-      return np;
-  }
-  
-  return NULL;
+    for (np = hashtab[hash_make(ipstr)]; np != NULL; np = np->next) {
+        if (!strcmp(np->ipstr, ipstr))
+            return np;
+    }
+
+    return NULL;
 }
 
 
 HASH *hash_install(HASH **hashtab, char *ipstr)
 {
-  HASH *np;
-  unsigned hashval;
+    HASH *np;
+    unsigned hashval;
 
-  if ((np = hash_lookup(hashtab, ipstr)) == NULL) {
-    np = malloc(sizeof(HASH));
-    strcpy(np->ipstr, ipstr);
-    np->count = 0;
-    hashval = hash_make(ipstr);
-    np->next = hashtab[hashval];
-    hashtab[hashval] = np;
-  } 
-  
-  return np;
+    if ((np = hash_lookup(hashtab, ipstr)) == NULL) {
+        np = malloc(sizeof(HASH));
+        strcpy(np->ipstr, ipstr);
+        np->count = 0;
+        hashval = hash_make(ipstr);
+        np->next = hashtab[hashval];
+        hashtab[hashval] = np;
+    }
+
+    return np;
 }
 
 
 HASH **hash_init()
 {
-  int j;
-  HASH **hashtab;
+    int j;
+    HASH **hashtab;
 
-  hashtab = malloc(HASHSIZE * sizeof(HASH));
-  for (j = 0 ; j < HASHSIZE ; j++)
-    hashtab[j] = NULL;
+    hashtab = malloc(HASHSIZE * sizeof(HASH));
+    for (j = 0; j < HASHSIZE; j++)
+        hashtab[j] = NULL;
 
-  return hashtab;
+    return hashtab;
 }
-
 
 
 HASH **hash_free(HASH **hashtab)
 {
-  unsigned long i;
-  HASH *data;
+    unsigned long i;
+    HASH *data;
 
-  if (hashtab == NULL)
-    return NULL;
+    if (hashtab == NULL)
+        return NULL;
 
-  for (i = 0 ; i < HASHSIZE ; i++) {
-    if (hashtab[i] == NULL)
-      continue;
-    for (data = hashtab[i] ; data != NULL ; ) { 
-      hashtab[i] = data->next;
-      free(data);
-      data = hashtab[i];
+    for (i = 0; i < HASHSIZE; i++) {
+        if (hashtab[i] == NULL)
+            continue;
+        for (data = hashtab[i]; data != NULL;) {
+            hashtab[i] = data->next;
+            free(data);
+            data = hashtab[i];
+        }
     }
-  }
-  free(hashtab);
-  hashtab = NULL;
+    free(hashtab);
+    hashtab = NULL;
 
-  return hashtab;
+    return hashtab;
 }
 
 
 HASH **hash_foreach(HASH **hashtab, void (*hashdo)(HASH *hashtab))
 {
-  unsigned long i;
-  HASH *data;
+    unsigned long i;
+    HASH *data;
 
-  if (hashtab == NULL)
-    return NULL;
+    if (hashtab == NULL)
+        return NULL;
 
-  for (i = 0 ; i < HASHSIZE ; i++) {
-    if (hashtab[i] == NULL)
-      continue;
-    for (data = hashtab[i] ; data != NULL ; ) { 
-      hashtab[i] = data->next;
-      (*hashdo)(data);
-      data = hashtab[i];
+    for (i = 0; i < HASHSIZE; i++) {
+        if (hashtab[i] == NULL)
+            continue;
+        for (data = hashtab[i]; data != NULL;) {
+            hashtab[i] = data->next;
+            (*hashdo)(data);
+            data = hashtab[i];
+        }
     }
-  }
 
-  return hashtab;
+    return hashtab;
 }
